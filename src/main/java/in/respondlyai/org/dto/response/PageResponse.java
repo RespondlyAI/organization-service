42f3768e1995d7ba5b215fc43f412c09ba126d89
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-04-08 12:54:17 +0530

    feat: create global pagination record

---
+++ src/main/java/in/respondlyai/org/dto/response/PageResponse.java
@@ -0,0 +1,12 @@
+package in.respondlyai.org.dto.response;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> content,
+        int pageNumber,
+        int pageSize,
+        long totalElements,
+        int totalPages,
+        boolean isLast
+) {}
