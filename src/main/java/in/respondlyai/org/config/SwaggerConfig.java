26211b9c9d969968da5a42153adb1213338e760f
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-04-10 09:33:39 +0530

    feat: add auth button for global token

---
+++ src/main/java/in/respondlyai/org/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package in.respondlyai.org.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Organization Service API", version = "v1", description = "Manages organizations and reference data"),
+        security = @SecurityRequirement(name = "bearerAuth") // Applies the token to all endpoints globally
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
